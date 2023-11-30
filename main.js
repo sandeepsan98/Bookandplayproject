88eccd2b0cc43d1b6061e166a31b86b5e89e138c
sandeepsan98 <152313486+sandeepsan98@users.noreply.github.com>
2023-11-30 14:28:43 +0530

    Commit files
    Commit files

---
+++ main.js
@@ -0,0 +1,388 @@
+if (JSON.parse(localStorage.getItem("ground_info"))) {
+  // return
+} else {
+  localStorage.setItem(
+    "ground_info",
+    JSON.stringify([
+      {
+        ground_Address:
+          "Sri rajpurohit complex, 1/340, West Ave, MKB Nagar, Vyasarpadi, Chennai, Tamil Nadu 600039",
+        ground_Place: "Vyasarpadi",
+        ground_city: "Chennai",
+        ground_id: 292,
+        ground_locat_link: "https://goo.gl/maps/YB4zbcWExPUTbGFT6",
+        ground_name: "DNS  TURF",
+        ground_price: "200",
+        ground_rules:
+          " For Football players are advised to wear shoes only on ground\n sharp metal football are not allowed For tennis 6 players are only allowed",
+        ground_timing_from: "10:00 AM",
+        ground_timing_to: "10:00 PM",
+        groundimg1: "https://iili.io/Hj2NLMJ.jpg",
+        groundimg2: "https://iili.io/Hj2NSaV.jpg",
+        groundimg3: "https://iili.io/Hj2NPZg.jpg",
+        sport_avail_1: true,
+        sport_avail_2: true,
+        sport_avail_3: false,
+        status: true,
+        groundCourt: "5",
+        groundIncreasePrice: "200",
+        seller_id: 149,
+      },
+      {
+        ground_Address:
+          "4th Floor, Hamid Building, No 190, Mount Rd, opp. IBIS Hotel, Chennai, Tamil Nadu 600006",
+        ground_Place: "Annasalai",
+        ground_city: "Chennai",
+        ground_id: 141,
+        ground_locat_link: "https://goo.gl/maps/BELGDCGXEGRoSGrV7",
+        ground_name: "Pitch and Play",
+        ground_price: "200",
+        ground_rules:
+          "For Football players are advised to wear shoes only on ground<\n sharp metal football are not allowed\n For tennis 6 players are only allowed",
+        ground_timing_from: "10:00 AM",
+        ground_timing_to: "10:00 PM",
+        groundimg1: "https://iili.io/HjPs46b.jpg",
+        groundimg2: "https://iili.io/HjPss8Q.jpg",
+        groundimg3: "https://iili.io/HjPsZAB.jpg",
+        sport_avail_1: true,
+        sport_avail_2: true,
+        sport_avail_3: true,
+        status: true,
+        groundCourt: "6",
+        groundIncreasePrice: "200",
+        seller_id: 142,
+      },
+      {
+        ground_Address:
+          "No. 95, New Avadi Rd, next to Kamalam Roxy, Thandavaraya, Kilpauk, Chennai, Tamil Nadu 600010",
+        ground_Place: "Kilpauk",
+        ground_city: "Chennai",
+        ground_id: 116,
+        ground_locat_link: "https://goo.gl/maps/4rnxHMxQaZ7JgFP6A",
+        ground_name: "Sports Sanity FC Marina",
+        ground_price: "300",
+        ground_rules:
+          "For Football players are advised to wear shoes only on ground\n   sharp metal football are not allowed \n   For tennis 6 players are only allowed",
+        ground_timing_from: "11:00 AM",
+        ground_timing_to: "10:00 PM",
+        groundimg1: "https://iili.io/HjLw6Je.jpg",
+        groundimg2: "https://iili.io/HjLwrg9.jpg",
+        groundimg3: "https://iili.io/HjLwP5u.jpg",
+        sport_avail_1: true,
+        sport_avail_2: true,
+        sport_avail_3: false,
+        status: true,
+        groundCourt: "2",
+        groundIncreasePrice: "200",
+        seller_id: 109,
+      },
+      {
+        ground_Address:
+          "B&C Mills, SPR City, Stephenson Rd, Perambur, Chennai, Tamil Nadu 600012",
+        ground_Place: "Perambur",
+        ground_city: "Coimbatore",
+        ground_id: 202,
+        ground_locat_link: "https://goo.gl/maps/XecciFUmrSkp2yff7",
+        ground_name: "HotFut SPR City",
+        ground_price: "200",
+        ground_rules:
+          "For Football players are advised to wear shoes only on ground\n   sharp metal football are not allowed \n For tennis 6 players are only allowed",
+        ground_timing_from: "1:00 AM",
+        ground_timing_to: "10:00 AM",
+        groundimg1: "https://iili.io/HjQK6mB.jpg",
+        groundimg2: "https://iili.io/HjQK4eV.jpg",
+        groundimg3: "https://iili.io/HjQK80b.jpg",
+        sport_avail_1: true,
+        sport_avail_2: true,
+        sport_avail_3: false,
+        status: true,
+        groundCourt: "2",
+        groundIncreasePrice: "200",
+        seller_id: 141,
+      },
+      {
+        ground_Address:
+          " No.131 - 133, M P M Road, Perambur, Chennai, Tamil Nadu 600039",
+        ground_Place: "Perambur",
+        ground_city: "Coimbatore",
+        ground_id: 106,
+        ground_locat_link: "https://goo.gl/maps/Mrk8pZV9i3ZHbdHFA",
+        ground_name: "Revolt theTurfWar",
+        ground_price: "300",
+        ground_rules:
+          "        For Football players are advised to wear shoes only on ground\n         sharp metal football are not allowed \n         For tennis 6 players are only allowed",
+        ground_timing_from: "11:00 AM",
+        ground_timing_to: "11:00 PM",
+        groundimg1: "https://iili.io/HwYWZvt.jpg",
+        groundimg2: "https://iili.io/HwYWkYP.jpg",
+        groundimg3: "https://iili.io/HwYWkYP.jpg",
+        sport_avail_1: true,
+        sport_avail_2: true,
+        sport_avail_3: false,
+        status: true,
+        groundCourt: "2",
+        groundIncreasePrice: "200",
+        seller_id: 159,
+      },
+      {
+        ground_Address:
+          "Kasi City Park-Kasi talkies, 4th Floor No.4, Pillaiyar Koil St, Kasi Estate, West Jafferkhanpet, Chennai, Tamil Nadu 600083",
+        ground_Place: "Kasi theatre",
+        ground_city: "Coimbatore",
+        ground_id: 36,
+        ground_locat_link: "https://goo.gl/maps/fHKX6ojbVwJSiKd2A",
+        ground_name: "Seven Eleven's Turf",
+        ground_price: "300",
+        ground_rules:
+          "  For Football players are advised to wear shoes only on ground\n         sharp metal football are not allowed \n         For tennis 6 players are only allowed",
+        ground_timing_from: "3:00 PM",
+        ground_timing_to: "10:00 PM",
+        groundimg1: "https://iili.io/HwanpVf.jpg",
+        groundimg2: "https://iili.io/HwanDts.jpg",
+        groundimg3: "https://iili.io/HwanQ9t.jpg",
+        sport_avail_1: false,
+        sport_avail_2: true,
+        sport_avail_3: true,
+        status: true,
+        groundCourt: "2",
+        groundIncreasePrice: "200",
+        seller_id: 129,
+      },
+      {
+        ground_Address:
+          "Lal Bagh Main Rd, Srinivas Colony, Sudhama Nagar, Bengaluru, Karnataka 560027",
+        ground_Place: "Bengaluru Sudhama Nagar",
+        ground_city: "Bangalore",
+        ground_id: 284,
+        ground_locat_link: "https://goo.gl/maps/bKYnyhsTqY4cqAKT7",
+        ground_name: "Tiento Sports",
+        ground_price: "200",
+        ground_rules:
+          "For Football players are advised to wear shoes only on ground\nsharp metal football are not allowed\nFor tennis 6 players are only allowed",
+        ground_timing_from: "10:00 PM",
+        ground_timing_to: "11:00 PM",
+        groundimg1: "https://iili.io/HwaDs4f.jpg",
+        groundimg2: "https://iili.io/HwaDtv2.jpg",
+        groundimg3: "https://iili.io/HwabHCu.jpg",
+        sport_avail_1: true,
+        sport_avail_2: true,
+        sport_avail_3: false,
+        status: true,
+        groundCourt: "2",
+        groundIncreasePrice: "200",
+        seller_id: 59,
+      },
+    ])
+  );
+}
+
+if (JSON.parse(localStorage.getItem("groundadmin_details"))) {
+  // return
+} else {
+  localStorage.setItem(
+    "groundadmin_details",
+    JSON.stringify([
+
+      {
+        imagename
+        : 
+        "",
+        seller_email
+        : 
+        "sandeepdoncsofficial@gmail.com",
+        seller_ground_name
+        : 
+        "dnf",
+        seller_id
+        : 
+        149,
+        seller_name
+        : 
+        "sandeep",
+        seller_password
+        : 
+        "@#(*Snsss",
+        seller_phoneno
+        : 
+        "1222222333",
+        url
+        : 
+        "",
+
+      },
+      {
+        imagename
+        : 
+        "",
+        seller_email
+        : 
+        "sandeepdondacsofficial@gmail.com",
+        seller_ground_name
+        : 
+        "dnf",
+        seller_id
+        : 
+        142,
+        seller_name
+        : 
+        "sandeep",
+        seller_password
+        : 
+        "@#(*Snsss",
+        seller_phoneno
+        : 
+        "1222222333",
+        url
+        : 
+        "",
+ 
+      },
+      {
+        imagename
+        : 
+        "",
+        seller_email
+        : 
+        "sandcsasofficial@gmail.com",
+        seller_ground_name
+        : 
+        "dnf",
+        seller_id
+        : 
+        109,
+        seller_name
+        : 
+        "sandeep",
+        seller_password
+        : 
+        "@#(*Snsss",
+        seller_phoneno
+        : 
+        "1222222333",
+        url
+        : 
+        "",
+
+      },
+      {
+        imagename
+        : 
+        "",
+        seller_email
+        : 
+        "sandy5tdoncsofficial@gmail.com",
+        seller_ground_name
+        : 
+        "dnf",
+        seller_id
+        : 
+        141,
+        seller_name
+        : 
+        "sandeep",
+        seller_password
+        : 
+        "@#(*Snsss",
+        seller_phoneno
+        : 
+        "1222222333",
+        url
+        : 
+        "",
+        
+      },
+      {
+        imagename
+        : 
+        "",
+        seller_email
+        : 
+        "tascsofficial@gmail.com",
+        seller_ground_name
+        : 
+        "dnf",
+        seller_id
+        : 
+        159,
+        seller_name
+        : 
+        "sandeep",
+        seller_password
+        : 
+        "@#(*Snsss",
+        seller_phoneno
+        : 
+        "1222222333",
+        url
+        : 
+        "",
+    
+      },
+      {
+        imagename
+        : 
+        "",
+        seller_email
+        : 
+        "sandeepdoncs@gmail.com",
+        seller_ground_name
+        : 
+        "dnf",
+        seller_id
+        : 
+        129,
+        seller_name
+        : 
+        "sandeep",
+        seller_password
+        : 
+        "@#(*Snsss",
+        seller_phoneno
+        : 
+        "1222222333",
+        url
+        : 
+        "",
+
+      },
+      {
+        imagename
+        : 
+        "",
+        seller_email
+        : 
+        "sandeeptark@gmail.com",
+        seller_ground_name
+        : 
+        "dnf",
+        seller_id
+        : 
+        59,
+        seller_name
+        : 
+        "sandeep",
+        seller_password
+        : 
+        "@#(*Snsss",
+        seller_phoneno
+        : 
+        "1222222333",
+        url
+        : 
+        "",
+
+      },
+
+    ])
+  )
+
+}
+
+// if (JSON.parse(localStorage.getItem("user_logged_in"))) {
+//   // return
+// } else {
+// }
+
+
+
+
